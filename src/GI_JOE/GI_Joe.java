f53c003215e4874536fb2b93b40b3c2dfc4bb04d
student <student@ip-172-31-50-74.us-west-2.compute.internal>
2021-08-23 18:25:25 -0700

    STORMSHADOW

--- src/GI_JOE/GI_Joe.java
+++ src/GI_JOE/GI_Joe.java
@@ -14,6 +14,8 @@ import javax.swing.SwingConstants;
 public class GI_Joe implements ActionListener {
 	String chosen;
 	int state = 0;
+	int nextState = 0;
+	JButton Next = new JButton();
 	JLabel label = new JLabel();
 	JButton Flint = new JButton();
 	JButton SnakeEyes = new JButton();
@@ -97,22 +99,42 @@ public class GI_Joe implements ActionListener {
 		panel = new JPanel();
 		frame.add(panel);
 		frame.setBounds(450, 0, 500, 100);
-		frame.setTitle("Your Mission if you choose to accept is this:");
+		frame.setTitle("Operation Desert Storm : Your Mission if you choose to accept is this:");
 		label = new JLabel();
 		label.setText("Infultrate the GI Joe Headquarters and descover their plans ");
-		JLabel l = new JLabel("<html>Hello World! <br/>blahblahblah<html>",SwingConstants.CENTER);
-				
+		JLabel l = new JLabel("<html>Mission Code: <br/>CODE/SHADOW<html>",SwingConstants.CENTER);
+		panel.add(l);
 		panel.add(label);
-		JButton Next = new JButton();
+		Next.addActionListener(this);
+		Next.setText("Start Mission!");
 		panel.add(Next);
 		//next
 		frame.setVisible(true);
 		frame.pack();
+		state=1;
+		
 	}
+	public void  stormShadowTwo() {
+		state =1;
+		nextState = 2;
+		frame.dispose();
+		frame = new JFrame();
+		panel = new JPanel();
+		frame.add(panel);
+		JOptionPane.showMessageDialog(null, ".");
+		JOptionPane.showMessageDialog(null, "..");
+		JOptionPane.showMessageDialog(null, "...");
+		JOptionPane.showMessageDialog(null, ".");
+		JOptionPane.showMessageDialog(null, "..");
+		JOptionPane.showMessageDialog(null, "Process complete!");
+		frame.setBounds(450, 0, 500, 100);
+		frame.isVisible();
+		
+		}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		int nextState = 0;
+		
 		switch (state) {
 		case 0:
 			nextState = characterConfirm((JButton) arg0.getSource());
@@ -120,14 +142,17 @@ public class GI_Joe implements ActionListener {
 		case 1:
 			break;
 		default:
-			JOptionPane.showMessageDialog(null, state + "State Not Found");
+			JOptionPane.showMessageDialog(null, state + " State Not Found");
 		}
 		switch (nextState) {
 		case 1:
 			stormShadowStart();
 			break;
+		case 2:
+			stormShadowTwo();
+			break;
 			default:
-				JOptionPane.showMessageDialog(null, nextState + "Next Statenot found");
+				JOptionPane.showMessageDialog(null, nextState + " Next State Not found");
 			
 		}
 		
